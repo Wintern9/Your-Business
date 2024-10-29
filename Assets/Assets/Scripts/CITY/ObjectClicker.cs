7a2a0db86da9f94216bc647c0688da6390275ba3
Wintern9 <egor20051303@gmail.com>
2024-10-29 17:37:21 +0300

    0.0.013
    Добавлена локация "GOVERMENT"

--- Assets/Assets/Scripts/CITY/ObjectClicker.cs
+++ Assets/Assets/Scripts/CITY/ObjectClicker.cs
@@ -1,1 +1,24 @@
-﻿using UnityEngine;  public class ObjectClicker : MonoBehaviour {     public Camera Camera;      void Update()     {         if (Input.GetMouseButtonUp(0))         {             Ray ray = Camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);             RaycastHit hit;              if (Physics.Raycast(ray, out hit))             {                 ClickableObject clickable = hit.collider.GetComponent<ClickableObject>();                 if (clickable != null)                 {                     clickable.OnClick();                 }             }         }     } } 
+﻿using UnityEngine;  public class ObjectClicker : MonoBehaviour {     [SerializeField] private bool Delay;     [SerializeField] private float TimeDelay;     public Camera Camera;     float Timer;      void Update()     {         if (Input.GetMouseButton(0))         {
+            if (Delay)
+            {
+                Timer += Time.deltaTime;
+            }
+            else if (Timer >= 10f)
+                Timer = 10f;
+            else
+            {
+                Timer = TimeDelay;
+            }
+        }          if (Input.GetMouseButtonUp(0))         {             if (Timer <= TimeDelay)
+            {
+                Ray ray = Camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    ClickableObject clickable = hit.collider.GetComponent<ClickableObject>();
+                    if (clickable != null)
+                    {
+                        clickable.OnClick();
+                    }
+                }             }             Timer = 0f;         }     } } 
